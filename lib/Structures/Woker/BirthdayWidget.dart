f9c9cf93b7ce5eae2b8e1f317fa40f76694c47e8
MAVRIN <soman@inbox.ru>
2020-04-29 15:34:42 +0300

    improved birthday widget

--- lib/Structures/Woker/BirthdayWidget.dart
+++ lib/Structures/Woker/BirthdayWidget.dart
@@ -13,17 +13,69 @@ class BirthdayWidget extends StatefulWidget {
 }
 
 class _BirthdayWidgetState extends State<BirthdayWidget> {
-  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('С днем рождения!'),
-      ),
-      body: Stack(
-        children: <Widget>[
-          Align(
+    return Stack(
+      children: <Widget>[
+        
+        Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            centerTitle: true,
+            title: Text('С днем рождения!'),
+          ),
+          body: Stack(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: widget.workers.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        Woker worker = widget.workers[index];
+                        return ListTile(
+                          title: Text(
+                            worker.name,
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context)
+                                .textTheme
+                                .title
+                                .copyWith(fontSize: 18),
+                          ),
+                          subtitle: Text(
+                            worker.position,
+                            textAlign: TextAlign.center,
+                          ),
+                          onTap: () {},
+                        );
+                      },
+                    ),
+                  ),
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: FlareActor(
+                      "assets/img/etc/cakes.flr",
+                      animation: 'Untitled',
+                    ),
+                  ))
+                ],
+              ),
+              Align(
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                      width: double.infinity,
+                      child: FlareActor(
+                        "assets/img/etc/sparks2.flr",
+                        alignment: Alignment.center,
+                        fit: BoxFit.contain,
+                        animation: 'Animations',
+                      ))),
+            ],
+          ),
+        ),
+        IgnorePointer(
+          child: Align(
               alignment: Alignment.bottomCenter,
               child: SizedBox(
                   width: double.infinity,
@@ -33,53 +85,8 @@ class _BirthdayWidgetState extends State<BirthdayWidget> {
                     fit: BoxFit.cover,
                     animation: 'Animations',
                   ))),
-          Column(
-            children: <Widget>[
-              Expanded(
-                child: ListView.builder(
-                  itemCount: widget.workers.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    Woker worker = widget.workers[index];
-                    return ListTile(
-                      title: Text(
-                        worker.name,
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context)
-                            .textTheme
-                            .title
-                            .copyWith(fontSize: 18),
-                      ),
-                      subtitle: Text(
-                        worker.position,
-                        textAlign: TextAlign.center,
-                      ),
-                      onTap: () {},
-                    );
-                  },
-                ),
-              ),
-              Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: FlareActor(
-                "assets/img/etc/cakes.flr",
-                animation: 'Untitled',
-              ),
-                  ))
-            ],
-          ),
-          Align(
-              alignment: Alignment.center,
-              child: SizedBox(
-                  width: double.infinity,
-                  child: FlareActor(
-                    "assets/img/etc/sparks2.flr",
-                    alignment: Alignment.center,
-                    fit: BoxFit.contain,
-                    animation: 'Animations',
-                  ))),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
